d76576582f813c188744f30e4a6d904170c55b94
phreezer <marcus.showalter@gmail.com>
2017-07-10 12:54:45 -0700

    Fixed .gitignore

---
+++ dist/js/jquery.i3g-template-loader.js
@@ -0,0 +1,73 @@
+/*!
+* i3g-template-loader - v0.0.2
+* Homepage: http://i-3global.com
+* Author: Heath Showalter
+* Author URL: http://heathshowalter.com/
+*/
+(function ($) {
+	'use strict';
+	/*jshint indent:4 */
+
+	$.serviceTemplateLoader = function () {
+
+		var serviceTemplateLoader = {},
+			config = {},
+			defaults = $.app.config,
+			cached = null,
+			$scope = {};
+
+
+
+		function getConfig(defaults, options) {
+			options = options || {};
+			defaults = defaults || {};
+			return $.extend(true, {}, defaults, options);
+		}
+
+
+
+		function renderHTML(data) {
+			data = data || '';
+			var template = Handlebars.compile(data);
+
+			return template($scope);
+		}
+
+
+		function getData() {
+			var deferred = Q.defer();
+
+			$.get(config.TEMPLATE_URL, function (data) {
+				cached = data;
+				deferred.resolve(renderHTML(data));
+			});
+
+			return deferred.promise;
+		}
+
+
+
+		function update(scope, options) {
+			config = getConfig(defaults, options);
+			$scope = scope || {};
+
+			config.TEMPLATE = config.TEMPLATE ? cached = config.TEMPLATE : false;
+
+			var deferred = Q.defer();
+			if (cached === null) {
+				deferred.resolve(getData());
+			} else {
+				deferred.resolve(renderHTML(cached));
+			}
+			deferred.resolve(getData());
+			return deferred.promise;
+		}
+
+
+		serviceTemplateLoader.update = update;
+
+		return serviceTemplateLoader;
+	};
+
+
+}(jQuery));
